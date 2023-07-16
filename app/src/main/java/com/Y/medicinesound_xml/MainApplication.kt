dbee89a26f9534b405cc43e528f2854cb896042c
Y <hwiyul_g@daum.net>
2023-07-16 16:34:47 +0900

    MODIFY & ADD : mvvm + hilt(di) 구조에 따라 위치 이동 + internet 권한 mainfest에 추가

--- app/src/main/java/com/Y/medicinesound_xml/MainApplication.kt
+++ app/src/main/java/com/Y/medicinesound_xml/MainApplication.kt
@@ -4,5 +4,10 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MedicineSoundApp : Application(){
+class MainApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
 }
